3c45fce80d71869d69ae2471b4e9f8c285d92b7d
Shahriar-H <67462321+Shahriar-H@users.noreply.github.com>
2022-08-30 09:22:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(titlePrev+newPosts);
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port number::${port}`);
+});
+
+
 
 const store = require('./rtk/app/store')
 const {fetchedPost} = require('./rtk/Feature/post/postSlice')
@@ -5,6 +19,7 @@ const {fetchedQueryPost} = require('./rtk/Feature/querypost/queryPostSlice')
 let state;
 store.subscribe(()=>{
     state= store.getState();   
+    
 })
 
 
@@ -13,4 +28,5 @@ store.dispatch(fetchedPost())
 // 
 setTimeout(() => {
     store.dispatch(fetchedQueryPost(state.post.posts.title))
+    console.log(state)
 }, 1000);
